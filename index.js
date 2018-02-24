b6fc5031850849ef3c2fd4046ae62cb6716e6a80
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-24 00:56:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ this.name = name
 this.homeState = homeState
 this.training = training
 }
-BoardMember.prototype.veto = function(){
+veto(){
   return 'No, I must disagree'
 }
 //
 
-BoardMember.prototype.approve = function(){
+approve(){
 return 'You can do that!'
 }
-BoardMember.prototype.doCharity = function(){
+doCharity(){
 return "I like to help people."
 }
 
-BoardMember.prototype.releasePressStatement = function(){
+releasePressStatement(){
   return "You will see great things from Scuber."
 }
 
-BoardMember.prototype.sayHi = function(){
+sayHi(){
 return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
 }
